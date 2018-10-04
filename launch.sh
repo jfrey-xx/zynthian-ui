931b16465532ca562b343d5dd77cf91cc141f9e7
jfrey <nomhad@gmail.com>
2018-10-04 13:53:21 +0200

    add test script

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export DISPLAY_WIDTH="320"
+export DISPLAY_HEIGHT="240"
+export ZYNTHIAN_AUBIONOTES=1
+export ZYNTHIAN_TOUCHOSC=1
+export ZYNTHIAN_WIRING_LAYOUT="EMULATOR"
+export ZYNTHIAN_LOG_LEVEL=10
+export ZYNTHIAN_MY_DATA_DIR=/home/jfrey/Bidouilles/zynthian/zynthian-my-data/
+export ZYNTHIAN_DATA_DIR=/home/jfrey/Bidouilles/zynthian/zynthian-data/
+./zynthian_gui.py
